b1795cf026c58fd386fc79378439a0945da3e4f2
riyapojage <133016157+riyapojage@users.noreply.github.com>
2024-03-04 19:10:36 +0530

    feat: added color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,45 +12,25 @@ export default {
 			},
 		},
 		extend: {
-			colors: {
-				border: 'hsl(var(--border))',
-				input: 'hsl(var(--input))',
-				ring: 'hsl(var(--ring))',
-				background: 'hsl(var(--background))',
-				foreground: 'hsl(var(--foreground))',
-				primary: {
-					DEFAULT: 'hsl(var(--primary))',
-					foreground: 'hsl(var(--primary-foreground))',
-				},
-				secondary: {
-					DEFAULT: 'hsl(var(--secondary))',
-					foreground: 'hsl(var(--secondary-foreground))',
-				},
-				destructive: {
-					DEFAULT: 'hsl(var(--destructive))',
-					foreground: 'hsl(var(--destructive-foreground))',
-				},
-				muted: {
-					DEFAULT: 'hsl(var(--muted))',
-					foreground: 'hsl(var(--muted-foreground))',
-				},
-				accent: {
-					DEFAULT: 'hsl(var(--accent))',
-					foreground: 'hsl(var(--accent-foreground))',
-				},
+			
 				popover: {
 					DEFAULT: 'hsl(var(--popover))',
 					foreground: 'hsl(var(--popover-foreground))',
 				},
-				card: {
-					DEFAULT: 'hsl(var(--card))',
-					foreground: 'hsl(var(--card-foreground))',
-				},
-			},
+				
+				colors: {
+					'color-primary': 'var(--baground-primary)',
+					'color-secondary': 'var(--text-color-secondary)',
+					'primary-yellow' : 'var(--accent-color-primary)',
+					'stroke-black-1': 'var(--text-color-primary)',
+					'stroke-black-2': 'var(--text-color-secondary)',
+
+					'btn-primary': 'var(--button-color-primary)',
+				  },
 			borderRadius: {
 				lg: 'var(--radius)',
-				md: 'calc(var(--radius) - 2px)',
-				sm: 'calc(var(--radius) - 4px)',
+				md: 'calc(var(--radius) - 8px)',
+				sm: 'calc(var(--radius) - 10px)',
 			},
 			keyframes: {
 				'accordion-down': {
