e996f2f5ba45a3093597cb026492986902ea53f3
anmol5936 <mangarajanmol666@gmail.com>
2024-03-07 14:04:05 +0530

    Refactor NavBar component to add responsive behavior

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,67 @@
+import { useState, useEffect } from 'react';
 import './NavBar.css';
 import NavElement from './shared/typography/NavElement';
 import NavButton from './shared/typography/NavButton';
 import Navigation from '../config/Navigation';
+import menuIcon from '../assets/images/menu.png';
+import unionIcon from '../assets/images/Union.png';
 
 function NavBar() {
 	const { navItems } = Navigation;
+	const [isMobile, setIsMobile] = useState(false);
+	const [isNavOpen, setIsNavOpen] = useState(false);
+
+	useEffect(() => {
+		const handleResize = () => {
+			setIsMobile(window.innerWidth < 1185);
+		};
+		window.addEventListener('resize', handleResize);
+		handleResize();
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+
+	const toggleNav = () => {
+		if (isMobile) {
+			setIsNavOpen(!isNavOpen);
+		}
+	};
 
 	return (
-		<div
-			className='navbar bg-center bg-cover bg-no-repeat'
-			style={{ backgroundImage: "url('./image.png')", background: 'black 0% 0% / 100px 100px repeat, #F9F5F2' }}>
-			<div className='navbar flex justify-between items-center px-[56.6px] py-[15.33px] opacity-53 bg-hard-light bg-center bg-cover bg-no-repeat'>
-				<div className='text-lg font-bold'>Logo</div>
-				<div className='flex gap-x-[55px]'>
+		<>
+			<div
+				className={`navbar flex justify-between items-center opacity-53 bg-hard-light bg-center bg-cover  h-[73.8px] overflow-hidden`}>
+				<div className='ml-[56.6px]'>Logo</div>
+				<div className={`navitem md:flex`}>
 					{navItems.map((item, index) => (
-						<NavElement key={index} className=''>
+						<NavElement key={index} className='mx-[55px]'>
 							{item.name}
 						</NavElement>
 					))}
 				</div>
 				<div className='flex'>
-					<NavButton className='login-button'>Log in</NavButton>
+					{isMobile && (
+						<NavButton className={`mr-[56.6px] hamburger-icon`} onClick={toggleNav}>
+							<img src={isNavOpen ? unionIcon : menuIcon} alt='Menu' />
+						</NavButton>
+					)}
+					{!isMobile && (
+						<NavButton className={`mr-[56.6px] login-button`} onClick={toggleNav}>
+							Login
+						</NavButton>
+					)}
 				</div>
 			</div>
-			<div className='line' style={{ width: '1440px', height: '1px', backgroundColor: 'black' }}></div>
-		</div>
+			<div className='w-full' style={{ height: '1px', backgroundColor: 'black' }}></div>
+			{isMobile && isNavOpen && (
+				<div className='navMobile flex flex-col justify-center bg-[#252525] items-center gap-[41px] h-[376px] text-white'>
+					{navItems.map((item, index) => (
+						<NavElement key={index} className='mx-[55px]'>
+							{item.name}
+						</NavElement>
+					))}
+				</div>
+			)}
+		</>
 	);
 }
 
