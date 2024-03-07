818124955ce094ed5c939b44b3b2f5cea5db6d5a
anmol5936 <mangarajanmol666@gmail.com>
2024-03-07 14:23:12 +0530

    minor changes

--- src/config/Navigation.js
+++ src/config/Navigation.js
@@ -1,9 +1,6 @@
 export default {
 	title: 'Navigation',
-	logo: {
-		src: 'https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/logo1_rg97i6.png',
-		alt: 'HackNITR 4.0',
-	},
+
 	navItems: [
 		{
 			id: 'About us',
