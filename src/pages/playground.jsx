878a655815fe002d6b9b8d183d104fd2fe53285a
Jayesh Nayak <starkjay2107@gmail.com>
2024-03-09 15:56:09 +0530

    feat: adding footer (#8)

--- src/pages/playground.jsx
+++ src/pages/playground.jsx
@@ -1,31 +1,36 @@
 import { Heading, HeroText, Paragraph, PersonalizedText, Text } from '../components/shared';
+import Footer from '../components/shared/marginals/footer.jsx';
 
 export default function Playground() {
 	return (
-		<div className='container py-6'>
-			<HeroText>Hero Text</HeroText>
-			<PersonalizedText>Personalized Text</PersonalizedText>
-			<Heading variant='h1'>Heading 1</Heading>
-			<Heading variant='h2'>Heading 2</Heading>
-			<Heading variant='h3'>Heading 3</Heading>
-			<Paragraph variant='body1'>Paragraph body1</Paragraph>
-			<Paragraph variant='body2'>Paragraph body2</Paragraph>
-			<Paragraph variant='body3'>Paragraph body3</Paragraph>
-			<Text variant='button'>Text button</Text>
-			<Text variant='nav'>Text nav</Text>
-			<Text variant='navButton'>Text navButton</Text>
-			<Text variant='footer'>Text footer</Text>
+		<>
+			<div className='container py-6'>
+				<HeroText>Hero Text</HeroText>
+				<PersonalizedText>Personalized Text</PersonalizedText>
+				<Heading variant='h1'>Heading 1</Heading>
+				<Heading variant='h2'>Heading 2</Heading>
+				<Heading variant='h3'>Heading 3</Heading>
+				<Paragraph variant='body1'>Paragraph body1</Paragraph>
+				<Paragraph variant='body2'>Paragraph body2</Paragraph>
+				<Paragraph variant='body3'>Paragraph body3</Paragraph>
+				<Text variant='button'>Text button</Text>
+				<Text variant='nav'>Text nav</Text>
+				<Text variant='navButton'>Text navButton</Text>
+				<Text variant='footer'>Text footer</Text>
 
-			<Heading variant='h1' className='mt-6'>
-				Colors
-			</Heading>
-			<ul>
-				<li className='text-primary'>Primary</li>
-				<li className='text-primary-foreground'>Primary Foreground</li>
-				<li className='text-primary-yellow'>Primary Yellow</li>
-				<li className='text-black'>Black</li>
-				<li className='text-black-foreground'>Black Foreground</li>
-			</ul>
-		</div>
+				<Heading variant='h1' className='mt-6'>
+					Colors
+				</Heading>
+				<ul>
+					<li className='text-primary'>Primary</li>
+					<li className='text-primary-foreground'>Primary Foreground</li>
+					<li className='text-primary-yellow'>Primary Yellow</li>
+					<li className='text-black'>Black</li>
+					<li className='text-black-foreground'>Black Foreground</li>
+				</ul>
+			</div>
+
+			<Footer />
+		</>
 	);
 }
