6c41ad5f19395f19e493589d3e82ec6261f207c2
Mixel2004 <84668201+Mixel2004@users.noreply.github.com>
2024-03-10 21:13:25 +0530

    feat: Initialize about us page

---
+++ src/data/aboutUsData.js
@@ -0,0 +1,29 @@
+const aboutUsData = {
+	tag1: "Let's relive & celebrate ",
+	tag2: 'our ',
+	tag3_char1: 'g',
+	tag3_wavy: 'ood day',
+	tag3: 's Together',
+	underline_img_url: 'https://i.ibb.co/mHmTkG7/underline.png',
+	lists: [
+		{
+			title: 'Rewind',
+			img_url: 'https://i.ibb.co/mBkgZsy/rewind.png',
+		},
+		{
+			title: 'Reunite',
+			img_url: 'https://i.ibb.co/Qrymm8f/reunite.png',
+		},
+		{
+			title: 'Rejoice',
+			img_url: 'https://i.ibb.co/LgrBJx3/rejoicÄe.png',
+		},
+	],
+	content1:
+		'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur',
+	content2:
+		'aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem',
+	star_img_url: 'https://i.ibb.co/hZ1mFDr/Star.png',
+};
+
+export default aboutUsData;
