f3ec2851a19f622817807d8f92b4f60f0485f8ca
RickDeb2004 <debanjanrick04@gmail.com>
2024-03-12 20:42:55 +0530

    Fix the alignment issue

--- src/components/shared/marginals/events.jsx
+++ src/components/shared/marginals/events.jsx
@@ -2,11 +2,11 @@ import { Heading, Text } from '../typography/index.jsx';
 
 export default function Event() {
 	return (
-		<div className=' p-8'>
+		<div className=' p-8' style={{ border: '1px solid red' }}>
 			{/* <div className='text-7xl font-bold text-[#d45500] flex items-center justify-center'>Events</div> */}
 
 			<Heading style={{ color: '#F2DA05', textAlign: 'center', textShadow: '4.608px 4.608px 0px #000' }}>Events</Heading>
-			<div className='flex justify-center mt-12 space-x-8'>
+			<div className='flex justify-between mt-12 space-x-8' style={{ border: '1px solid blue' }}>
 				<div
 					className='bg-[#ffcccb] p-6 rounded-lg w-80 relative'
 					style={{
@@ -14,11 +14,17 @@ export default function Event() {
 						border: '1px solid #000',
 						background: '#FF9E7C',
 						boxShadow: '4px 4px 0px 0px #000',
-						gap: '32px',
+						gap: '64px',
 					}}>
 					<div
-						className='bg-[#ff8c69] text-white px-3 py-1 rounded-full text-sm absolute -mt-10 ml-60'
-						style={{ borderRadius: '9px', border: '2px solid #000', background: 'var(--Accent-Yellow, #F2DA05)' }}>
+						className='bg-[#ff8c69] text-white px-3 py-1 rounded-full text-sm absolute -mt-10 ml-60  '
+						style={{
+							borderRadius: '9px',
+							border: '2px solid #000',
+							background: 'var(--Accent-Yellow, #F2DA05)',
+							whiteSpace: 'nowrap',
+							width: 'auto',
+						}}>
 						4PM onward
 					</div>
 					<h3 className='text-xl font-semibold mb-2'>
@@ -49,11 +55,17 @@ export default function Event() {
 						border: '1px solid #000',
 						background: '#FF9E7C',
 						boxShadow: '4px 4px 0px 0px #000',
-						gap: '32px',
+						gap: '64px',
 					}}>
 					<div
-						className='bg-[#ff8c69] text-white px-3 py-1 rounded-full text-sm absolute -mt-10 ml-60'
-						style={{ borderRadius: '9px', border: '2px solid #000', background: 'var(--Accent-Yellow, #F2DA05)' }}>
+						className='bg-[#ff8c69] text-white px-3 py-1 rounded-full text-sm absolute -mt-10 ml-60  '
+						style={{
+							borderRadius: '9px',
+							border: '2px solid #000',
+							background: 'var(--Accent-Yellow, #F2DA05)',
+							whiteSpace: 'nowrap',
+							width: 'auto',
+						}}>
 						9AM to 10:30PM
 					</div>
 					<h3 className='text-xl font-semibold mb-2'>
@@ -84,11 +96,17 @@ export default function Event() {
 						border: '1px solid #000',
 						background: '#FF9E7C',
 						boxShadow: '4px 4px 0px 0px #000',
-						gap: '32px',
+						gap: '64px',
 					}}>
 					<div
-						className='bg-[#ff8c69] text-white px-3 py-1 rounded-full text-sm absolute -mt-10 ml-60'
-						style={{ borderRadius: '9px', border: '2px solid #000', background: 'var(--Accent-Yellow, #F2DA05)' }}>
+						className='bg-[#ff8c69] text-white px-3 py-1 rounded-full text-sm absolute -mt-10 ml-60 '
+						style={{
+							borderRadius: '9px',
+							border: '2px solid #000',
+							background: 'var(--Accent-Yellow, #F2DA05)',
+							whiteSpace: 'nowrap',
+							width: 'auto',
+						}}>
 						9AM to 2PM
 					</div>
 					<h3 className='text-xl font-semibold mb-2'>
