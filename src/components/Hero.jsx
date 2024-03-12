16164750f5034c07ecb8ece5c8ad3a18982b9b05
aryab2003 <bhattacharyyaarya165@gmail.com>
2024-03-12 22:39:06 +0530

    fix:made tw changes in laptop view and started adding responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,9 +28,9 @@ const Hero = () => {
 						</button>
 					</div>
 				</div>
-				<div className='w-[100%] md:w-1/2 flex items-center relative' style={{ height: '600px', border: '1px solid navy' }}>
+				<div className='w-[100%] md:w-1/2 flex items-center relative' style={{ height: '600px' }}>
 					<div
-						className='absolute top-50 left-10 '
+						className='absolute top-50 right-200'
 						style={{
 							borderRadius: '6px',
 							border: '1px solid #000',
