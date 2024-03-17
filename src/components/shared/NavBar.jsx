3c2aa2b526cf7522c355e5887886e0ecd5aa304b
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2024-03-17 23:41:12 +0530

    chore: float the navbar

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -18,7 +18,7 @@ function NavBar() {
 	return (
 		<nav style={{ position: 'sticky', top: '0' }}>
 			<div
-				className='navbar flex justify-between items-center opacity-53 bg-hard-light bg-center bg-cover bg-[#F9F5F2] h-[73.8px] overflow-hidden pl-10 z-1'
+				className='navbar flex justify-between items-center opacity-53 bg-hard-light bg-center bg-cover bg-[#F9F5F2] h-[73.8px] overflow-hidden pl-10 z-20'
 				style={{ backgroundImage: `url(${image})` }}>
 				<Logo src={logo.src} alt={logo.alt} />
 				<div className={`navitem md:flex ${isNavOpen ? 'hidden' : 'flex'} hidden`}>
