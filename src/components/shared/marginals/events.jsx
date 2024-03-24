5c7e0a0e3523ed8feb129bd6c645f1d022a3cd6f
RickDeb2004 <debanjanrick04@gmail.com>
2024-03-24 13:14:06 +0530

    Fixed: typography for event time & modified inline styling & used HeroText1 for Events heading

--- src/components/shared/marginals/events.jsx
+++ src/components/shared/marginals/events.jsx
@@ -1,46 +1,38 @@
-import { Heading, Text } from '../typography/index.jsx';
+import { Heading, Paragraph, HeroText1 } from '../typography/index.jsx';
 import eventData from '../../../data/eventData.js';
 
 export default function Event() {
 	return (
 		<div className='p-8 border-1 border-red-500 '>
-			<Heading
-				variant='h1'
-				className='text-primary-yellow text-center text-5xl md:text-6xl lg:text-7xl'
+			<HeroText1
+				className='text-primary-yellow text-center '
 				style={{
 					textShadow: '4.608px 4.608px 0px #000',
 					WebkitTextStrokeWidth: 2.71052622795105,
 					WebkitTextStrokeColor: '#252525',
 				}}>
 				Events
-			</Heading>
+			</HeroText1>
 			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-12'>
 				{eventData.map((event, index) => (
 					<div
 						key={index}
-						className='bg-[#ffcccb] p-6 rounded-lg flex-shrink-0 relative'
+						className='p-6 rounded-lg flex-shrink-0 relative w-auto bg-[#FF9E7C]'
 						style={{
 							borderRadius: '8px',
 							border: '1px solid #000',
-							background: '#FF9E7C',
+
 							boxShadow: '4px 4px 0px 0px #000',
-							width: 'auto',
 						}}>
 						<div
-							className='bg-[#ff8c69] text-white px-3 py-1 rounded-full text-sm absolute  '
+							className=' text-white px-3 py-1 rounded-full text-sm absolute  -top-3.5 -right-4 z-10  whitespace-nowrap w-auto bg-yellow-300'
 							style={{
-								top: '-0.5rem',
-								right: '-1.5rem',
 								borderRadius: '9px',
 								border: '2px solid #000',
-								background: 'var(--Accent-Yellow, #F2DA05)',
-								whiteSpace: 'nowrap',
-								width: 'auto',
-								fontSize: '14px',
-
-								zIndex: '10',
 							}}>
-							{event.time}
+							<Paragraph variant='body3' className='text-black'>
+								{event.time}
+							</Paragraph>
 						</div>
 						<h3 className='text-xl font-semibold mb-2'>
 							<Heading variant='h3'>{event.date}</Heading>
@@ -51,7 +43,9 @@ export default function Event() {
 						<ul className='text-[#d45500]'>
 							{event.agenda.map((item, index) => (
 								<li key={index}>
-									<Text style={{ color: '#000', fontSize: '16px' }}>{item}</Text>
+									<Paragraph variant='body3' className='text-black'>
+										{item}
+									</Paragraph>
 								</li>
 							))}
 						</ul>
